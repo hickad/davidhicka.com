90108e7675ae17886eece20c2d89e9952526adff
hickad <hickad@gmail.com>
2024-07-20 18:08:35 -0400

    Added the contact form

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ import '../plugins/tiny-slider/tiny-slider.js';
 import '../js/dark-mode.js';
 import './blog.js';
 import './header.js';
-
+import './masks.js';
 
 
 
---
+++ assets/js/masks.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var phoneInput = document.getElementById('your-phone');
+
+    phoneInput.addEventListener('input', function(e) {
+        var value = phoneInput.value.replace(/\D/g, '');
+        var formattedValue = '';
+
+        if (value.length > 0) {
+            formattedValue += '(' + value.substring(0, 3);
+        }
+        if (value.length >= 4) {
+            formattedValue += ') ' + value.substring(3, 6);
+        }
+        if (value.length >= 7) {
+            formattedValue += '-' + value.substring(6, 10);
+        }
+
+        phoneInput.value = formattedValue;
+    });
+
+    phoneInput.addEventListener('blur', function() {
+        var value = phoneInput.value.replace(/\D/g, '');
+        if (value.length !== 10) {
+            phoneInput.value = '';
+        }
+    });
+});
