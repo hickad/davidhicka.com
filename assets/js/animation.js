3cec7a5f6eb707f6f14a71e3f23e497d2539df50
hickad <hickad@gmail.com>
2024-08-04 16:28:57 -0400

    Fixed animation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -8,10 +8,9 @@ AOS.init({
     delay: 0,
   });
 
-
   document.addEventListener('DOMContentLoaded', function () {
     const progressBars = document.querySelectorAll('.progress-bar');
-    
+
     function isElementInViewport(el) {
         const rect = el.getBoundingClientRect();
         return (
@@ -22,24 +21,34 @@ AOS.init({
         );
     }
 
+    function animateProgressBar(bar) {
+        const percentage = parseInt(bar.getAttribute('data-percentage'), 10);
+        let width = 0;
+        const increment = 2; // Increase the width by 2% each step
+        const interval = setInterval(() => {
+            if (width >= percentage) {
+                clearInterval(interval);
+            } else {
+                width += increment;
+                if (width > percentage) width = percentage; // Ensure it doesn't exceed the percentage
+                bar.style.width = width + '%';
+                bar.querySelector('strong').textContent = width + '%';
+            }
+        }, 10); // Decrease the interval to 10ms
+    }
+
     function animateProgressBars() {
         progressBars.forEach(bar => {
-            if (isElementInViewport(bar)) {
-                const percentage = bar.getAttribute('data-percentage');
-                if (percentage !== null) {
-                    bar.style.width = percentage + '%';
-                } else {
-                    console.error('No data-percentage attribute found on element:', bar);
-                }
+            if (isElementInViewport(bar) && !bar.classList.contains('animated')) {
+                animateProgressBar(bar);
+                bar.classList.add('animated');
             }
         });
     }
 
-    // Initial check
-    animateProgressBars();
+    // Initial check after the page is fully loaded
+    window.addEventListener('load', animateProgressBars);
 
     // Check on scroll
     window.addEventListener('scroll', animateProgressBars);
 });
-
-
